5f129c7669c953fa4bc94bdd1ae70cbe72c7b884
Dmitry Egorov <egorovdima60@gmail.com>
2022-08-25 18:11:18 +0200

    feat: add HomeBloc

--- lib/home/bloc/home_event.dart
+++ lib/home/bloc/home_event.dart
@@ -1,4 +1,13 @@
 part of 'home_bloc.dart';
 
 @immutable
-abstract class HomeEvent {}
+abstract class HomeEvent extends Equatable {
+  const HomeEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class HomeLoaded extends HomeEvent {
+  const HomeLoaded();
+}
--- lib/home/bloc/home_state.dart
+++ lib/home/bloc/home_state.dart
@@ -1,6 +1,43 @@
 part of 'home_bloc.dart';
 
-@immutable
-abstract class HomeState {}
+class Login extends Equatable {
+  const Login({
+    required this.userName,
+    required this.serviceName,
+  });
 
-class HomeInitial extends HomeState {}
+  final String userName;
+  final String serviceName;
+
+  @override
+  List<Object?> get props => [userName, serviceName];
+}
+
+@immutable
+abstract class HomeState extends Equatable {
+  const HomeState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class HomeInitial extends HomeState {
+  const HomeInitial();
+}
+
+class HomeLoadInProgress extends HomeState {
+  const HomeLoadInProgress();
+}
+
+class HomeLoadSuccess extends HomeState {
+  const HomeLoadSuccess({required this.logins});
+
+  final List<Login> logins;
+
+  @override
+  List<Object?> get props => [logins];
+}
+
+class HomeLoadFailure extends HomeState {
+  const HomeLoadFailure();
+}
