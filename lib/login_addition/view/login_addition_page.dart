0616bfe59100f228fa540de74bc647c0e4b4fd60
Dmitry Egorov <egorovdima60@gmail.com>
2022-08-25 21:01:57 +0200

    feat: implement login addition page

--- lib/login_addition/view/login_addition_page.dart
+++ lib/login_addition/view/login_addition_page.dart
@@ -20,8 +20,18 @@ class LoginAdditionPage extends StatelessWidget {
   }
 }
 
-class _LoginAdditionView extends StatelessWidget {
+class _LoginAdditionView extends StatefulWidget {
   const _LoginAdditionView();
+
+  @override
+  State<_LoginAdditionView> createState() => _LoginAdditionViewState();
+}
+
+class _LoginAdditionViewState extends State<_LoginAdditionView> {
+  final _formKey = GlobalKey<FormState>();
+  final _serviceNameFieldController = TextEditingController();
+  final _userNameFieldController = TextEditingController();
+  final _passwordFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,51 +39,135 @@ class _LoginAdditionView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('New Login'),
       ),
-      body: ListView(
-        children: [
-          Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.business),
-                    labelText: 'Service Name',
-                  ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person),
-                    labelText: 'User Name',
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.password),
-                    labelText: 'Password',
-                  ),
-                ),
-              ],
-            ),
+      body: MultiBlocListener(
+        listeners: [
+          BlocListener<LoginAdditionBloc, LoginAdditionState>(
+            listener: (context, state) => _handleSaveSuccess(),
+            listenWhen: (previous, current) {
+              return current is LoginAdditionSaveSuccess;
+            },
           ),
-          const SizedBox(height: 30),
-          Column(
-            children: [
-              SizedBox(
-                width: 150,
-                child: FloatingActionButton.extended(
-                  onPressed: _handleAddButtonPress,
-                  icon: const Icon(Icons.add),
-                  label: const Text('Add Login'),
-                ),
-              ),
-            ],
+          BlocListener<LoginAdditionBloc, LoginAdditionState>(
+            listener: (context, state) => _handleSaveFailure(),
+            listenWhen: (previous, current) {
+              return current is LoginAdditionSaveFailure;
+            },
           ),
         ],
+        child: ListView(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(children: _buildFields()),
+            ),
+            const SizedBox(height: 30),
+            _buildAddButton(),
+          ],
+        ),
       ),
     );
   }
 
-  void _handleAddButtonPress() {}
+  List<Widget> _buildFields() {
+    final isSaving =
+        context.watch<LoginAdditionBloc>().state is LoginAdditionSaveInProgress;
+    return [
+      TextFormField(
+        enabled: !isSaving,
+        controller: _serviceNameFieldController,
+        validator: _validateField,
+        decoration: const InputDecoration(
+          prefixIcon: Icon(Icons.business),
+          labelText: 'Service Name',
+        ),
+      ),
+      TextFormField(
+        enabled: !isSaving,
+        controller: _userNameFieldController,
+        validator: _validateField,
+        decoration: const InputDecoration(
+          prefixIcon: Icon(Icons.person),
+          labelText: 'User Name',
+        ),
+      ),
+      TextFormField(
+        enabled: !isSaving,
+        obscureText: true,
+        enableSuggestions: false,
+        controller: _passwordFieldController,
+        validator: _validateField,
+        decoration: const InputDecoration(
+          prefixIcon: Icon(Icons.password),
+          labelText: 'Password',
+        ),
+      ),
+    ];
+  }
+
+  Widget _buildAddButton() {
+    return Column(
+      children: [
+        SizedBox(
+          width: 150,
+          child: BlocBuilder<LoginAdditionBloc, LoginAdditionState>(
+            builder: (context, state) {
+              if (state is LoginAdditionSaveInProgress) {
+                return FloatingActionButton.extended(
+                  onPressed: () {},
+                  backgroundColor: Colors.grey,
+                  icon: const CircularProgressIndicator(),
+                  label: const Text('Saving...'),
+                );
+              }
+              return FloatingActionButton.extended(
+                onPressed: _handleAddButtonPress,
+                icon: const Icon(Icons.add),
+                label: const Text('Add Login'),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  String? _validateField(String? value) {
+    if (value != null && value.isNotEmpty) {
+      return null;
+    }
+    return 'Incorrect value!';
+  }
+
+  void _handleAddButtonPress() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    context.read<LoginAdditionBloc>().add(
+          LoginAdditionCompleted(
+            serviceName: _serviceNameFieldController.text,
+            userName: _userNameFieldController.text,
+            password: _passwordFieldController.text,
+          ),
+        );
+  }
+
+  void _handleSaveSuccess() {
+    Navigator.pop(context);
+  }
+
+  void _handleSaveFailure() {
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(
+        const SnackBar(content: Text('Failed to save login!')),
+      );
+  }
+
+  @override
+  void dispose() {
+    _serviceNameFieldController.dispose();
+    _userNameFieldController.dispose();
+    _passwordFieldController.dispose();
+    super.dispose();
+  }
 }
