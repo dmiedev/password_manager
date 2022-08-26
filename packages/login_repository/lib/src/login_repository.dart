f5cc6d818e668eecbdd75a03f289ae68ed9b93e3
Dmitry Egorov <egorovdima60@gmail.com>
2022-08-26 14:31:30 +0200

    feat: add login saving and loading

--- packages/login_repository/lib/src/login_repository.dart
+++ packages/login_repository/lib/src/login_repository.dart
@@ -1,3 +1,51 @@
+import 'package:hive/hive.dart';
+import 'package:login_repository/src/models/models.dart';
+
 class LoginRepository {
-  const LoginRepository();
+  LoginRepository({Box<Login>? box}) : _box = box {
+    _registerAdapters();
+  }
+
+  static const _boxName = 'logins';
+  Box<Login>? _box;
+
+  void _registerAdapters() {
+    _registerAdapter(LoginAdapter());
+  }
+
+  void _registerAdapter<T>(TypeAdapter<T> adapter) {
+    if (!Hive.isAdapterRegistered(adapter.typeId)) {
+      Hive.registerAdapter(adapter);
+    }
+  }
+
+  Future<void> initialize() async {
+    _box = await Hive.openBox(_boxName);
+  }
+
+  void _checkIsInitialized() {
+    if (_box == null) {
+      throw FilterRepositoryNotInitializedException();
+    }
+  }
+
+  List<Login> get logins {
+    _checkIsInitialized();
+    try {
+      return _box!.values.toList();
+    } on Exception {
+      throw Exception();
+    }
+  }
+
+  Future<void> saveLogin(Login login) {
+    _checkIsInitialized();
+    try {
+      return _box!.add(login);
+    } on Exception {
+      throw Exception();
+    }
+  }
 }
+
+class FilterRepositoryNotInitializedException implements Exception {}
